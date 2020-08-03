64dc27b13f2ad9a13ce1e3fcd4fa7b5b1bcfed95
ginnyTheCat <ginnythecat@lelux.net>
2020-08-03 12:17:56 +0200

    Made keyboard input working;

--- lib/player.dart
+++ lib/player.dart
@@ -2,7 +2,10 @@ import 'package:thoughts/physics/object.dart';
 import 'package:thoughts/world/location.dart';
 
 class Player extends PhysicsObject {
+  static const WALK_VELOCITY = 10;
+
   int jumped = 0;
+  bool running = false;
 
   int hearts = 3;
 
@@ -14,5 +17,39 @@ class Player extends PhysicsObject {
     if (grounded()) {
       jumped = 0;
     }
+  }
+
+  void jump() {
+    if (running) {
+      return;
+    }
+    if (jumped == 0) {
+      velocity.y -= 5.0 * gravitySide.forceMult;
+    } else if (jumped == 1) {
+      toggleGravity();
+    }
+    jumped += 1;
+  }
+
+  void run(bool toRight) {
+    if (running) {
+      return;
+    }
+    running = true;
+    if (toRight) {
+      velocity.x += WALK_VELOCITY;
+    } else {
+      velocity.x -= WALK_VELOCITY;
+    }
+  }
+
+  void brake() {
+    running = false;
+    velocity.x = 0;
+  }
+
+  void toggleGravity() {
+    gravitySide =
+        gravitySide == GravitySide.top ? GravitySide.bottom : GravitySide.top;
   }
 }
